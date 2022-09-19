9289e8a826ab76a1c2ccff9d5700c4b9b9a7eaad
Aakash Dhal <45394245+KRITIKALai@users.noreply.github.com>
2022-09-19 11:42:02 +0530

    Update countDigits.cpp

--- Step1/Know Basic Maths/countDigits.cpp
+++ Step1/Know Basic Maths/countDigits.cpp
@@ -1,1 +1,28 @@
-sample
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        return floor(log10(N)+1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
