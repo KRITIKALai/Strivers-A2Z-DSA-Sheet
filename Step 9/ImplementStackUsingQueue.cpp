f50aa5fd62b17536ad5865235b2035ee32d91fdc
Aakash Dhal <45394245+KRITIKALai@users.noreply.github.com>
2022-11-16 12:26:13 +0530

    Create ImplementStackUsingQueue.cpp

---
+++ Step 9/ImplementStackUsingQueue.cpp
@@ -0,0 +1,23 @@
+void QueueStack :: push(int x)
+{
+        // Your Code
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+}
+
+//Function to pop an element from stack using two queues. 
+int QueueStack :: pop()
+{
+        // Your Code
+        if(q1.empty()) return -1;
+        int val = q1.front();
+        q1.pop();
+        return val;
+}
